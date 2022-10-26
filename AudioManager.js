28cf1e32ef293067f17521961bdc30afe6a164d5
Bryan Berger <bryan@bryanberger.com>
2022-10-26 10:53:48 -0400

    add some stuff

--- AudioManager.js
+++ AudioManager.js
@@ -1,25 +1,33 @@
 // const Speaker = require('speaker')
-const path = require('path')
+const path = require("path");
 // const fs = require('fs-extra')
-const wae = require('@descript/web-audio-js')
-const AudioContext = wae.StreamAudioContext
+const wae = require("@descript/web-audio-js");
+const AudioContext = wae.StreamAudioContext;
 // const { v4: uuidv4 } = require('uuid')
-const { Track } = require('./Track')
+const { Track } = require("./Track");
+
+const RAND_MIN = 0.085;
+const RAND_MAX = 0.5;
+const REMIX_INTERVAL = 30000;
 
 class AudioManager {
   constructor() {
     // this._cache = {}
-    this._tracks = []
-    this._context = null
+    this._tracks = [];
+    this._context = null;
     this._sourceTracks = [
-      path.join(__dirname + '/audio/BARMAN_WORKING.wav'),
-      path.join(__dirname + '/audio/FULL_ROOM.wav'),
-      path.join(__dirname + '/audio/NIGHT_AMBIENCE.wav'),
-      path.join(__dirname + '/audio/PEOPLE_TALKING.wav'),
-      path.join(__dirname + '/audio/RAIN_ON_WINDOW.wav'),
-      path.join(__dirname + '/audio/SERVING_DRINKS.wav'),
-      path.join(__dirname + '/audio/STREET_AMBIENCE.wav'),
-    ]
+      // path.join(__dirname + "/audio/BARMAN_WORKING.wav"),
+      path.join(__dirname + "/audio/FULL_ROOM.wav"),
+      path.join(__dirname + "/audio/NIGHT_AMBIENCE.wav"),
+      path.join(__dirname + "/audio/PEOPLE_TALKING.wav"),
+      path.join(__dirname + "/audio/RAIN_ON_WINDOW.wav"),
+      // path.join(__dirname + '/audio/SERVING_DRINKS.wav'),
+      path.join(__dirname + "/audio/STREET_AMBIENCE.wav"),
+      path.join(__dirname + "/audio/ESCALATOR.wav"),
+      path.join(__dirname + "/audio/JUNGLE.wav"),
+      path.join(__dirname + "/audio/PIGEONS.wav"),
+      path.join(__dirname + "/audio/NATURE.wav"),
+    ];
   }
 
   start() {
@@ -27,51 +35,58 @@ class AudioManager {
     // fs.ensureDirSync(path.join(__dirname, '/audioCache'))
 
     // Master context
-    this._context = new AudioContext({ sampleRate: 48000 })
+    this._context = new AudioContext({ sampleRate: 48000 });
 
     // Master gain
-    const gain = this._context.createGain()
-    gain.connect(this._context.destination)
-    gain.gain.value = 0.3
+    const gain = this._context.createGain();
+    gain.connect(this._context.destination);
+    gain.gain.value = 0.3;
 
     // Add tracks at random volumes
     this._sourceTracks.forEach((path) => {
-      const track = new Track(this._context, path, Math.random(0.1, 0.6))
-      track.load()
-      track.connect(gain)
+      const track = new Track(
+        this._context,
+        path,
+        Math.random(RAND_MIN, RAND_MAX)
+      );
+      track.load();
+      track.connect(gain);
 
-      this._tracks.push(track)
-    })
-
-    // Simulate remixing volume every 20s
-    // setInterval(() => this.remix, 20000)
+      this._tracks.push(track);
+    });
 
     // Pipe output
     // this._context.pipe(new Speaker())
     // this._context.resume()
 
-    return this._context
+    return this._context;
+  }
+
+  startTimer() {
+    // Simulate remixing volume every 30s
+    setInterval(this.remix.bind(this), REMIX_INTERVAL);
   }
 
   setOutputStream(stream) {
-    this._context.suspend()
-    this._context.pipe(stream)
-    this._context.resume()
+    this._context.suspend();
+    this._context.pipe(stream);
+    this._context.resume();
 
-    console.log('Audio output stream changed')
+    console.log("Audio output stream changed");
   }
 
   remix() {
-    if (this._tracks.length > 0) {
+    if (this._tracks && this._tracks.length > 0) {
+      console.log("remixing!");
       this._tracks.forEach((track) => {
-        track.volume = Math.random(0.1, 0.6)
-      })
+        track.volume = Math.random(RAND_MIN, RAND_MAX);
+      });
     }
   }
 
   get tracks() {
-    return this._tracks
+    return this._tracks;
   }
 }
 
-module.exports = AudioManager
+module.exports = AudioManager;
--- index.js
+++ index.js
@@ -1,126 +1,137 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const { PassThrough } = require('stream')
-const AudioManager = require('./AudioManager')
+require("dotenv").config();
+const Discord = require("discord.js");
+const { PassThrough } = require("stream");
+const AudioManager = require("./AudioManager");
 
-const client = new Discord.Client()
-const audioManager = new AudioManager()
-const stream = new PassThrough()
+const client = new Discord.Client({
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"],
+});
+const audioManager = new AudioManager();
+const stream = new PassThrough();
 
 const trackNames = [
-  'BARMAN_WORKING',
-  'FULL_ROOM',
-  'NIGHT_AMBIENCE',
-  'PEOPLE_TALKING',
-  'RAIN_ON_WINDOW',
-  'SERVING_DRINKS',
-  'STREET_AMBIENCE',
-]
+  "BARMAN_WORKING",
+  "FULL_ROOM",
+  "NIGHT_AMBIENCE",
+  "PEOPLE_TALKING",
+  "RAIN_ON_WINDOW",
+  "SERVING_DRINKS",
+  "STREET_AMBIENCE",
+  "ESCALATOR",
+  "JUNGLE",
+  "PIGEONS",
+  "NATURE",
+];
 
 client
-  .on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-    audioManager.start()
+  .on("ready", async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    audioManager.start();
   })
-  .on('message', async (message) => {
-    const args = message.content.toLowerCase().split(' ')
-    if (args[0] === 'xjoin') {
-      console.log('command:', args)
+  .on("message", async (message) => {
+    const args = message.content.toLowerCase().split(" ");
+    if (args[0] === "xjoin") {
+      console.log("command:", args);
       try {
         if (!message.member.voice.channel) {
           return message.reply(
-            'You need to be in a voice channel to summon me.',
-          )
+            "You need to be in a voice channel to summon me."
+          );
         }
-        const channel = await message.member.voice.channel.join()
-        await broadcast(channel)
+        const channel = await message.member.voice.channel.join();
+        await broadcast(channel);
       } catch (err) {
-        return message.reply('I need permission to join your voice channel.')
+        return message.reply("I need permission to join your voice channel.");
       }
-    } else if (args[0] === 'xleave') {
-      console.log('command:', args)
-      const channel = message.member.voice.channel
+    } else if (args[0] === "xleave") {
+      console.log("command:", args);
+      const channel = message.member.voice.channel;
       if (
         channel &&
         message.client.voice.connections.some(
-          (connection) => connection.channel === channel,
+          (connection) => connection.channel === channel
         )
       ) {
-        channel.leave()
+        channel.leave();
       } else {
-        return message.reply("I'm not in your voice channel.")
+        return message.reply("I'm not in your voice channel.");
       }
-    } else if (args[0] === 'xrejoin') {
-      const channel = message.member.voice.channel
+    } else if (args[0] === "xrejoin") {
+      const channel = message.member.voice.channel;
 
       if (!channel) {
-        return message.reply('You need to be in a voice channel.')
+        return message.reply("You need to be in a voice channel.");
       }
       try {
-        channel.leave()
-        await sleep(1000)
-        await channel.join()
+        channel.leave();
+        await sleep(1000);
+        await channel.join();
       } catch (err) {
-        return message.reply('I need permission to join your voice channel.')
+        return message.reply("I need permission to join your voice channel.");
       }
-    } else if (args[0] === 'xtracks') {
-      console.log('command:', args)
-      return message.reply(trackNames.join(', '))
-    } else if (args[0] === 'xgain') {
-      console.log('command:', args)
-      if(args.length < 3) {
-        return message.reply('Bad command format. Example: xgain FULL_ROOM 0.5')
+    } else if (args[0] === "xtracks") {
+      console.log("command:", args);
+      return message.reply(trackNames.join(", "));
+    } else if (args[0] === "xgain") {
+      console.log("command:", args);
+      if (args.length < 3) {
+        return message.reply(
+          "Bad command format. Example: xgain FULL_ROOM 0.5"
+        );
       }
 
       if (trackNames.includes(args[1].toUpperCase())) {
-        const trackName = args[1].toUpperCase()
-        const gain = Number(args[2])
+        const trackName = args[1].toUpperCase();
+        const gain = Number(args[2]);
 
         if (!isNaN(gain) && gain >= 0 && gain <= 1) {
           // set gain for this track if it matches
           audioManager.tracks.forEach((track) => {
-            const trackHumanName = track.name.replace('.wav', '').trim()
+            const trackHumanName = track.name.replace(".wav", "").trim();
             if (trackHumanName === trackName) {
-              track.volume = gain
-              return message.reply(`Gain value for ${trackName} set to ${gain}`)
+              track.volume = gain;
+              return message.reply(
+                `Gain value for ${trackName} set to ${gain}`
+              );
             }
-          })
+          });
         } else {
-          return message.reply('Gain value is not a number from 0 to 1')
+          return message.reply("Gain value is not a number from 0 to 1");
         }
       } else {
-        return message.reply('Track name does not exist...')
+        return message.reply("Track name does not exist...");
       }
       // gain per channel
-    } else if (message.content === 'xremix') {
-      console.log('command:', args)
-      audioManager.remix()
-      return message.reply('Track gains remixed, takes effect in 4 seconds')
+    } else if (message.content === "xremix") {
+      console.log("command:", args);
+      audioManager.remix();
+      return message.reply("Track gains remixed, takes effect in 4 seconds");
     }
   })
-  .on('error', console.error)
-  .on('warn', console.warn)
-  .on('debug', console.log)
+  .on("error", console.error)
+  .on("warn", console.warn)
+  .on("debug", console.log);
 
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
 
 const broadcast = async (channel) => {
   try {
-    const broadcast = client.voice.createBroadcast()
+    const broadcast = client.voice.createBroadcast();
 
-    broadcast.play(stream, { type: 'converted' })
-    broadcast.on('error', (err) => console.log(err))
-    broadcast.on('start', () => console.log(`start`))
-    broadcast.on('finish', () => console.log(`finish`))
+    broadcast.play(stream, { type: "converted" });
+    broadcast.on("error", (err) => console.log(err));
+    broadcast.on("start", () => console.log(`start`));
+    broadcast.on("finish", () => console.log(`finish`));
 
-    audioManager.setOutputStream(stream)
+    audioManager.setOutputStream(stream);
+    audioManager.startTimer();
 
-    channel.play(broadcast)
+    channel.play(broadcast);
   } catch (e) {
-    console.log(`Error connecting to voice channel: ${e}`)
+    console.log(`Error connecting to voice channel: ${e}`);
   }
-}
+};
 
 const sleep = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
